724efd2909786a62f628d065ddf2badb1f8591db
Medha <medha.agarwal@austere.co.in>
2024-06-24 19:33:20 +0530

    Added login/register page, made search bar working, creates different todos for different users, handled exceptions

--- app.py
+++ app.py
@@ -1,40 +1,110 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'  # Add a secret key for session management
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    todos = db.relationship('Todo', backref='user', lazy=True)
 
 class Todo(db.Model):
-    sno = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(200), nullable = False)
-    desc = db.Column(db.String(500), nullable = False)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"{self.sno} - {self.title}"
 
-@app.route('/', methods = ['GET', 'POST'])
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Check if username already exists
+        user_exists = User.query.filter_by(username=username).first()
+        if user_exists:
+            flash('Username already exists. Please choose a different one.')
+            return redirect(url_for('register'))
+
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        flash('Registration successful! Please log in.', 'success')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('hello_world'))
+        else:
+            flash('Login unsuccessful. Check username and password', 'danger')
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/search', methods=['GET'])
+@login_required
+def search():
+    search_query = request.args.get('search_query', '')
+    if search_query:
+        # Perform search query based on current_user.id and search_query
+        filtered_todos = Todo.query.filter(
+            Todo.user_id == current_user.id,
+            (Todo.title.contains(search_query)) | (Todo.desc.contains(search_query))
+        ).all()
+    else:
+        filtered_todos = []
+
+    return render_template('search_results.html', filtered_todos=filtered_todos, search_query=search_query)
+
+
+@app.route('/', methods=['GET', 'POST'])
+@login_required
 def hello_world():
     if request.method == 'POST':
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo(title = title, desc = desc)
+        todo = Todo(title=title, desc=desc, user_id=current_user.id)
         db.session.add(todo)
         db.session.commit()
-        
-    allTodo = Todo.query.all()
+
+    allTodo = Todo.query.filter_by(user_id=current_user.id).all()
     return render_template('index.html', allTodo=allTodo)
 
-@app.route('/show')
-def products():
-    allTodo = Todo.query.all()
-    print(allTodo)
-    return 'this is products page'
-
-@app.route('/update/<int:sno>', methods = ['GET', 'POST'])
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+@login_required
 def update(sno):
     if request.method == 'POST':
         title = request.form['title']
@@ -42,7 +112,6 @@ def update(sno):
         todo = Todo.query.filter_by(sno=sno).first()
         todo.title = title
         todo.desc = desc
-        db.session.add(todo)
         db.session.commit()
         return redirect("/")
 
@@ -50,13 +119,12 @@ def update(sno):
     return render_template('update.html', todo=todo)
 
 @app.route('/delete/<int:sno>')
+@login_required
 def delete(sno):
     todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect('/')
 
-
-
 if __name__ == "__main__":
-    app.run(debug=True, port = 8000)
+    app.run(debug=True, port=8000)
